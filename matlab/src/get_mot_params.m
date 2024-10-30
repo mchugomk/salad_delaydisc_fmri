dcf1c7f5d0a61059ee551c33542c84ec5b44f57d
Maureen McHugo <maureen.mchugo@cuanschutz.edu>
2024-10-30 13:41:56 -0600

    update

--- matlab/src/get_mot_params.m
+++ matlab/src/get_mot_params.m
@@ -8,7 +8,7 @@ function [rp_file, dvars_file, fd_file]=get_mot_params(confound_file)
 rp_file=fullfile(confound_path, ['rp_' confound_name '.txt']);                      % output file for realignment params
 dvars_file=fullfile(confound_path, ['dvars_' confound_name '.txt']);                % output file for dvars
 fd_file=fullfile(confound_path, ['fd_' confound_name '.txt']);                      % output file for fd
-% mot_summary_file=fullfile(confound_path, ['mot_summary_' confound_name '.txt']);    % output file for motion summary
+mot_summary_file=fullfile(confound_path, ['mot_summary_' confound_name '.txt']);    % output file for motion summary
 
 % Specify columns to extract from confound file
 rp_cols=["trans_x", "trans_y", "trans_z", "rot_x", "rot_y", "rot_z"];   % spm style realignment columns
@@ -33,18 +33,18 @@ writematrix(fd_data, fd_file);
 % Save summary stats for motion
 % calc minimum, maximum, mean, sd, and median for realignment params, dvars, and fd
 % - omitnan bc of dvars, fd
-% mot_data=[rp_data dvars_data fd_data];
-% mot_stat_names={'min'; 'max'; 'mean'; 'sd'; 'median'};
-% mot_min=min(mot_data, [], 1, 'omitnan');
-% mot_max=max(mot_data, [], 1, 'omitnan');
-% mot_mean=mean(mot_data, 'omitnan');
-% mot_sd=std(mot_data, 'omitnan');
-% mot_median=median(mot_data, 'omitnan');
-% mot_stats=[mot_min; mot_max; mot_mean; mot_sd; mot_median];
-% 
-% mot_stats_table=array2table(mot_stats, ...
-%         'VariableNames', mot_cols, ...
-%         'RowNames', mot_stat_names);
-% writetable(mot_stats_table, mot_summary_file, 'Delimiter', '\t', 'WriteRowNames', true);
+mot_data=[rp_data dvars_data fd_data];
+mot_stat_names={'min'; 'max'; 'mean'; 'sd'; 'median'};
+mot_min=min(mot_data, [], 1, 'omitnan');
+mot_max=max(mot_data, [], 1, 'omitnan');
+mot_mean=mean(mot_data, 'omitnan');
+mot_sd=std(mot_data, 'omitnan');
+mot_median=median(mot_data, 'omitnan');
+mot_stats=[mot_min; mot_max; mot_mean; mot_sd; mot_median];
+
+mot_stats_table=array2table(mot_stats, ...
+        'VariableNames', mot_cols, ...
+        'RowNames', mot_stat_names);
+writetable(mot_stats_table, mot_summary_file, 'Delimiter', '\t', 'WriteRowNames', true);
 
 end
